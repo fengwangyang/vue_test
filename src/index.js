a14943d6e4a4d1d1d2aab63805f530056e1f2c0c
xujia <164221501@qq.com>
2020-11-23 21:48:39 +0800

    提交

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {render} from './render/index' 
+import {render} from '../render/index.js' 
 
 export default function wangyang_vue(){
     this.render = render;
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+module.exports = {
+  mode: "development", // "production" | "development" | "none"  // 告诉webpack是生产环境还是开发环境.
+  entry: "./src/index.js", // string | object | array  // 默认 ./src
+  // 入口起点，可以指定多个入口起点
+  output: {
+    // 输出，只可指定一个输出配置
+    path: path.resolve(__dirname, "dist"), // string
+    //  所有输出文件所在的目录
+    // 必须是绝对路径(use the Node.js path module)
+    filename: "index.js", // string    // 输出文件的名称
+    // library: "Vue", // string,
+    // //导出库的名称
+    // libraryTarget: "umd", // universal module definition    // the type of the exported library
+  },
+}
